5849e229a1d70624780390a43d2d5f9ff02ef8d2
Jonathan Holloway <loadtheaccumulator@gmail.com>
2016-06-23 10:46:36 -0500

    add pythonstartup_script
    Signed-off-by: Jonathan Holloway <loadtheaccumulator@gmail.com>

--- examples/pythonstartup_script.py
+++ examples/pythonstartup_script.py
@@ -2,5 +2,6 @@ from glusto.core import Glusto as g
 
 print "Python startup starting"
 config = g.load_config('examples/systems.yml')
+g.update_config(config)
 rcode, uname_info, rerr = g.run(config['nodes'][0], 'uname -a')
 print "Python startup complete"
