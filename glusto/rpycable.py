3a12501f418dc563e6780c9ee6c0f3778210af9b
Jonathan Holloway <loadtheaccumulator@gmail.com>
2016-06-24 01:45:37 -0500

    rpycable: minor edit
    Signed-off-by: Jonathan Holloway <loadtheaccumulator@gmail.com>

--- glusto/rpycable.py
+++ glusto/rpycable.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Jonathan Holloway <loadtheaccumulator@gmail.com>
+# Copyright 2016 Jonathan Holloway <loadtheaccumulator@gmail.com>
 #
 # This module is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
